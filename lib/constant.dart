e61e466f9ea3613fb4cd33fc849080e636152596
Muhammad Bilal <muhammadbilaltanoli07@gmail.com>
2023-08-21 23:30:01 +0500

    Final Commit

---
+++ lib/constant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tealColor = Colors.teal;
+
+const Color blueColor = Colors.blue;
+
+const Color lightColor = Colors.tealAccent;
+
+const headingTextStyle = TextStyle(color: Colors.white, fontSize: 20);
